6b92677ebc34236cd5d51a22bbc785846d541446
Russell Fraze <russell.fraze@gmail.com>
2023-02-10 14:22:49 -0800

    steps selected styled

--- src/components/Steps.js
+++ src/components/Steps.js
@@ -12,9 +12,11 @@ function Steps() {
             <div className={`${styles.step} ${step === 1 ? styles.selected : ''}`}>1</div>
             <div className={`${styles.step} ${step === 2 ? styles.selected : ''}`}>2</div>
             <div className={`${styles.step} ${step === 3 ? styles.selected : ''}`}>3</div>
-            <div className={`${styles.step} ${step === 4 ? styles.selected : ''}`}>4</div>
+            <div className={`${styles.step} ${step === 4 || step === 5 ? styles.selected : ''}`}>4</div>
         </div>
     )
 }
 
 export default Steps
+
+
