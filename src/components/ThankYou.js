67043ca6270c40beab0f47b515b953a697e759af
Russell Fraze <russell.fraze@gmail.com>
2023-02-26 13:41:37 -0800

    thank you styling

--- src/components/ThankYou.js
+++ src/components/ThankYou.js
@@ -1,17 +1,21 @@
-import {ReactComponent as ThankYouIcon} from '../assets/icon-thank-you.svg'
+import Card from '../components/UI/Card'
+import { ReactComponent as ThankYouIcon } from '../assets/icon-thank-you.svg'
+import styles from './ThankYou.module.css'
 
 
 
 function ThankYou() {
     return (
-        <div>
-            <ThankYouIcon />
-            <h1>Thank you!</h1>
-            <p>Thanks for confirming your subscription! 
+        <Card>
+            <div className={styles.thankYou}>
+                <ThankYouIcon height={55} />
+                <h1>Thank you!</h1>
+                <p>Thanks for confirming your subscription!
                 We hope you have fun using out platform. If you need
                 support, please feel free to email us at support@loremgaming.com.
-            </p>
-        </div>
+                </p>
+            </div>
+        </Card>
     )
 }
 
