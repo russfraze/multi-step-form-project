74daa231318fc091ea505b0b9480478cee74bd7b
Russell Fraze <russell.fraze@gmail.com>
2023-02-27 16:06:40 -0800

    steps desktop layout

--- src/components/Steps.js
+++ src/components/Steps.js
@@ -1,18 +1,42 @@
 import styles from './Steps.module.css'
 import FormContext from '../context/FormContext'
-import {useContext} from 'react'
+import { useContext } from 'react'
 
 function Steps() {
     const formCXT = useContext(FormContext)
 
-    const {step} = formCXT
+    const { step } = formCXT
 
     return (
         <div className={styles.steps}>
-            <div className={`${styles.step} ${step === 1 ? styles.selected : ''}`}>1</div>
-            <div className={`${styles.step} ${step === 2 ? styles.selected : ''}`}>2</div>
-            <div className={`${styles.step} ${step === 3 ? styles.selected : ''}`}>3</div>
-            <div className={`${styles.step} ${step === 4 || step === 5 ? styles.selected : ''}`}>4</div>
+            <div className={styles.stepGroup}>
+                <div className={`${styles.step} ${step === 1 ? styles.selected : ''}`}>1</div>
+                <div className={styles.stepText}>
+                    <p>STEP 1</p>
+                    <h2>YOUR INFO</h2>
+                </div>
+            </div>
+            <div className={styles.stepGroup}>
+                <div className={`${styles.step} ${step === 2 ? styles.selected : ''}`}>2</div>
+                <div className={styles.stepText}>
+                    <p>STEP 2</p>
+                    <h2>SELECT PLAN</h2>
+                </div>
+            </div>
+            <div className={styles.stepGroup}>
+                <div className={`${styles.step} ${step === 3 ? styles.selected : ''}`}>3</div>
+                <div className={styles.stepText}>
+                    <p>STEP 3</p>
+                    <h2>ADD-ONS</h2>
+                </div>
+            </div>
+            <div className={styles.stepGroup}>
+                <div className={`${styles.step} ${step === 4 || step === 5 ? styles.selected : ''}`}>4</div>
+                <div className={styles.stepText}>
+                    <p>STEP 4</p>
+                    <h2>SUMMARY</h2>
+                </div>
+            </div>
         </div>
     )
 }
