44f7f67a947d2c05a69866b6dc1881bc7ee05a37
Russell Fraze <russell.fraze@gmail.com>
2023-02-28 16:44:08 -0800

    basic form validation and v1 finished

--- src/components/PersonalInfoForm.js
+++ src/components/PersonalInfoForm.js
@@ -1,11 +1,17 @@
 import FormContext from '../context/FormContext'
-import { useContext, useRef } from 'react'
+import { useContext, useRef, useState } from 'react'
 import Button from './UI/Button'
 import Card from './UI/Card'
 import styles from './PersonalInfoForm.module.css'
 
 function PersonalInfoForm() {
     const formCXT = useContext(FormContext)
+    const [enteredNameIsValid, setEnteredNameIsValid] = useState(true)
+    const [enteredEmailIsValid, setEnteredEmailIsValid] = useState(true)
+    const [enteredPhoneIsValid, setEnteredPhoneIsValid] = useState(true)
+    
+
+
 
     const { next, setPersonalInfo } = formCXT
 
@@ -17,9 +23,30 @@ function PersonalInfoForm() {
 
     const handleSubmit = (e) => {
         e.preventDefault()
+
         const enteredName = nameInputRef.current.value
         const enteredEmail = emailInputRef.current.value
         const enteredPhone = phoneInputRef.current.value
+
+
+        if(enteredName.trim() === ''){
+            setEnteredNameIsValid(false)
+            return
+        }
+
+        setEnteredNameIsValid(true)
+
+        if(enteredEmail.trim() === ''){
+            setEnteredEmailIsValid(false)
+            return
+        }
+
+        setEnteredEmailIsValid(true)
+
+        if(enteredPhone.trim() === '' || enteredPhone.trim().length < 10){
+            setEnteredPhoneIsValid(false)
+            return
+        }
 
         setPersonalInfo({
             name: enteredName,
@@ -31,6 +58,10 @@ function PersonalInfoForm() {
 
 
     }
+
+    // const inputStyles = 'invalid'
+    const nameInputStyles = enteredNameIsValid ? '' : 'invalid'
+    const emailInputStyles = enteredEmailIsValid ? '' : 'invalid'
 
     return (
 
@@ -47,12 +78,14 @@ function PersonalInfoForm() {
                             <h1>Personal info</h1>
                             <p>Please provide your name, email address, and phone number.</p>
 
-                            <div className='flexGroup__col'>
+                            <div className={`${'flexGroup__col'} ${nameInputStyles}`}>
+                                {enteredNameIsValid ? '' : <label className={styles.error}>This field is required</label>}
                                 <label htmlFor='name'>Name</label>
                                 <input ref={nameInputRef} type='text' id='name' placeholder='e.g. Stephen King' />
                             </div>
 
-                            <div className='flexGroup__col'>
+                            <div className={`${'flexGroup__col'} ${emailInputStyles}`}>
+                                {enteredEmailIsValid ? '' : <label className={styles.error}>This field is required</label>}
                                 <label>Email Address</label>
                                 <input ref={emailInputRef} type='email' id='email' placeholder='e.g. stephenking@lorem.com' />
                             </div>
